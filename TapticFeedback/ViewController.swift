ccddf2626757ff564eacea03ddbd947104f08366
Zewu Chen <contact.zewu@gmail.com>
2019-08-23 15:16:57 -0300

    add functions

--- TapticFeedback/ViewController.swift
+++ TapticFeedback/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tapticsuttons:[UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func erroButton(_ sender: Any) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.error)
+    }
 
+    @IBAction func sucessoButton(_ sender: Any) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    @IBAction func perigoButton(_ sender: Any) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.warning)
+    }
+    
+    @IBAction func lightButton(_ sender: Any) {
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+    
+    @IBAction func mediumButton(_ sender: Any) {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+    
+    @IBAction func heavyButton(_ sender: Any) {
+        let generator = UIImpactFeedbackGenerator(style: .heavy)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+    
+    @IBAction func selecaoButton(_ sender: Any) {
+        let generator = UISelectionFeedbackGenerator()
+        generator.selectionChanged()
+    }
 }
 
