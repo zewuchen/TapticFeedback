9d3c20bfa72df769ec5918ffdd5020548430bdf9
Zewu Chen <contact.zewu@gmail.com>
2019-08-23 15:00:15 -0300

    first commit

---
+++ TapticFeedback/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapticFeedback
+//
+//  Created by Zewu Chen on 23/08/19.
+//  Copyright © 2019 Zewu Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
